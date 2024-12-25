4052626c9220b94f8a9bb28513ddbb5ed702ff37
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2024-12-26 00:45:23 +0600

    callback function tested

---
+++ Nodes - Behind the Scenes [Overview]/callback.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+// const data = fs.readFileSync("example.txt");
+
+// console.log(data.toString());
+
+fs.readFile("example.txt", (error, data) => {
+    if (error){
+       return console.log(error);
+    }
+
+    console.log(data.toString());
+
+});
+console.log("Program ended");
